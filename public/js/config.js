97747ba635aed213f9206fc2270588680d4c7f27
Kevin Simper <kevin.simper@gmail.com>
2014-04-28 20:36:42 +0200

    Filter which converts object to array

--- public/js/config.js
+++ public/js/config.js
@@ -2,3 +2,13 @@ var app = require('./modules/app');
 
 app.value('firebaseUrl', 'https://mylessons.firebaseio.com/')
 app.constant('loginRedirectPath', '/')
+
+app.filter('array', function() {
+  return function(items) {
+    var filtered = [];
+    angular.forEach(items, function(item) {
+      filtered.push(item);
+    });
+   return filtered;
+  };
+});
