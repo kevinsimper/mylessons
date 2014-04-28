8d19b5ffe2d15dd2be95b6e66eb81a4ae4db4fe7
Kevin Simper <kevin.simper@gmail.com>
2014-04-28 20:56:34 +0200

    Store default values one place

--- public/js/config.js
+++ public/js/config.js
@@ -3,6 +3,11 @@ var app = require('./modules/app');
 app.value('firebaseUrl', 'https://mylessons.firebaseio.com/')
 app.constant('loginRedirectPath', '/')
 
+app.value('LessonDefaultValues', {
+	'types': ['video', 'article', 'event'],
+	'levels': ['basic', 'medium', 'advanced']
+})
+
 app.filter('array', function() {
   return function(items) {
     var filtered = [];
