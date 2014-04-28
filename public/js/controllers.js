d1427d289a95fc4ce28e5cac317555e287f236db
Kevin Simper <kevin.simper@gmail.com>
2014-04-28 20:05:46 +0200

    Add a default thumbnail and shorten name if too long

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -159,19 +159,31 @@ var app = require('./modules/app')
 
 .controller('LessonsListCtrl', ['$scope', 'Lessons', '$location', function($scope, Lessons, $location){
   Lessons.all.$bind($scope, 'lessons');
-
+  var temp = 'http://www.ac4d.com/blog/uploads/2011/04/jonKolko.jpg';
   $scope.getStyles = function(lesson) {
     if(lesson.picture){
       return {
-        'background': 'url(' + lesson.thumbnail + ')'
+        'background': 'url(' + lesson.thumbnail + ')',
+        'background-size': '100%'
       };
     } else {
-      return {};
+      return {
+        'background': 'url(' + temp + ')',
+        'background-size': '100%'
+      };
     }
   };
 
   $scope.goToLesson = function(id, name) {
     $location.path('/lessons/' + id);
+  };
+
+  $scope.shortenLessonname = function(name) {
+    if(name.length < 55){
+      return name;
+    } else {
+      return name.substring(0, 55) + '..';
+    }
   };
 
 }])
