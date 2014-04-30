76e467205d8d54cdf7aba2c4d7a94fc4b08a3daf
Kevin Simper <kevin.simper@gmail.com>
2014-04-30 21:38:34 +0200

    Working filter function

--- public/js/config.js
+++ public/js/config.js
@@ -1,23 +1,30 @@
 var app = require('./modules/app');
+var _ = require('underscore');
 
 app.value('firebaseUrl', 'https://mylessons.firebaseio.com/')
 app.constant('loginRedirectPath', '/')
 
 app.value('LessonDefaultValues', {
-  'types': [{'display': 'Video', 'value': 'video'}, 
+  'types': [{'display': 'Video', 'value': 'video'},
             {'display': 'Article', 'value': 'article'},
             {'display': 'Event', 'value': 'event'}],
-  'levels': [{'display': 'Basic', 'value': 'basic'}, 
-             {'display': 'Medium', 'value': 'medium'}, 
+  'levels': [{'display': 'Basic', 'value': 'basic'},
+             {'display': 'Medium', 'value': 'medium'},
              {'display': 'Advanced', 'value': 'advanced'}]
 })
 
-app.filter('array', function() {
-  return function(items) {
-    var filtered = [];
-    angular.forEach(items, function(item) {
-      filtered.push(item);
-    });
-   return filtered;
+
+app.filter('filterObjects', function(){
+  return function(items, filters, reverse){
+    if(_.isEmpty(filters)) return items;
+    var data = {};
+    for(item in items){
+      for(filter in filters){
+        if(items[item][filter] === filters[filter]){
+          data[item] = items[item];
+        }
+      }
+    }
+    return data;
   };
 });
