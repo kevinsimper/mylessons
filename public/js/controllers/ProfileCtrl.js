b10cf0cfd3cc8c97f2aa67251e0c9d1bd15cfcd4
Kevin Simper <kevin.simper@gmail.com>
2014-05-29 16:14:38 +0200

    Show how long since the user is last seen

--- public/js/controllers/ProfileCtrl.js
+++ public/js/controllers/ProfileCtrl.js
@@ -1,9 +1,14 @@
 var angular = require('angular');
 var app = require('../modules/app');
+var moment = require('moment');
 
 app.controller('ProfileCtrl', ['$scope', '$rootScope', '$routeParams', '$location', 'User', function($scope, $rootScope, $routeParams, $location, User){
 	var userId = $routeParams.user;
 	$scope.profilePicture = userId.split(':')[1];
 	$scope.user = User.$child(userId);
+	$scope.user.$on('loaded', function(){
+		$scope.user.seenAgo = moment($scope.user.lastLogin).fromNow();
+	});
+
 
 }]);
